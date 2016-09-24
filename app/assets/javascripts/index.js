1635ea2cd5f1ad4f4aacc20ca14595dd5e07e9f9
Raphael Barbo <rsbarbo@gmail.com>
2016-09-24 15:15:21 -0600

    now ideas can be qualified (up and down)

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,22 +1,67 @@
 $(document).ready(() => {
-  console.log('hello');
-  getIdeas()
+  const index = new Index()
+  index.handleUpdateQuality()
 })
 
-const appendTo = ideas => {
-  $('#ideas').html(ideas)
-}
+class Index {
+  constructor() {
+    this.up = {
+      swill: "plausible",
+      plausible: 'genius',
+      genius: 'genius'
+    }
+    this.down = {
+      genius: 'plausible',
+      plausible: 'swill',
+      swill: 'swill'
+    }
+    this.ideas = []
+    this.getIdeas()
+  }
 
-const createIdeas = ideas => {
-  return ideas.map(idea => {
-    return `<h1>${idea.title}</h1><h2>${idea.body}</h2>`
-  }).join('')
-}
+  getIdeas() {
+    $.getJSON('/api/v1/ideas')
+      .then(response => {
+        this.ideas = response.sort((a, b) => a.id < b.id)
+        const validHTML = this.createIdeas(response)
+        this.appendTo(validHTML)
+      })
+  }
 
-const getIdeas = () => {
-  $.getJSON('/api/v1/ideas')
-    .then(response => {
-      const validHTML = createIdeas(response)
-      appendTo(validHTML)
+  handleUpdateQuality(e) {
+    $('#ideas').on('click', e => {
+      const ideaId = +e.target.parentElement.dataset.id
+      const idea = this.ideas.filter(e => e.id === ideaId)[0]
+      const buttonText = e.target.textContent
+      if (buttonText === 'Up') idea.quality = this.up[idea.quality]
+      if (buttonText === 'Down') idea.quality = this.down[idea.quality]
+      $.ajax({
+        type: "PATCH",
+        url: `/api/v1/ideas/${ideaId}`,
+        data: idea,
+        success: response => {
+          this.getIdeas()
+        }
+      });
     })
+  }
+
+  createIdeas() {
+    return this.ideas.map(idea =>
+      `
+      <div id="idea" data-id=${idea.id}>
+      <h1>${idea.title}</h1>
+      <h2>${idea.body}</h2>
+      <h3>${idea.quality}</h3>
+      <button>Up</button>
+      <button>Down</button>
+      <hr>
+      </div>
+      `
+    ).join('')
+  }
+
+  appendTo(ideas) {
+    $('#ideas').html(ideas)
+  }
 }
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,4 +4,16 @@ class Api::V1::IdeasController < ApplicationController
     render json: Idea.all
   end
 
+  def update
+    @idea = Idea.find(params[:id])
+    render json: @idea.update(idea_params)
+  end
+
+
+  private
+
+  def idea_params
+    params.permit(:title, :body, :quality)
+  end
+
 end
